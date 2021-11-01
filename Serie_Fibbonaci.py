e924179062844af5feb22eb1cb08caad23c822d1
Diego Tamayo <diegot1998@gmail.com>
2021-10-31 19:49:39 -0500

    Recursive Fibbonaci

---
+++ Serie_Fibbonaci.py
@@ -0,0 +1,18 @@
+# funcion para el n numero fibonacci
+ 
+def Fibonacci(n):
+    if n<0:
+        print("Incorrect input")
+    # El primer numero fibonacci es 0
+    elif n==0:
+        return 0
+    # El segundo numero fibonacci es 1
+    elif n==1:
+        return 1
+    else:
+        return Fibonacci(n-1)+Fibonacci(n-2)
+ 
+
+ 
+print(Fibonacci(9))
+ 
